934f56e4f3617896dc1899f15d3b0aa3e11cbca7
iki1uc <entertrainer@web.de>
2026-08-02 07:29:15 +0200

    Update NCLinkFullSync.js

--- WLOCH/NCLinkFullSync.js
+++ WLOCH/NCLinkFullSync.js
@@ -1,10 +1,8 @@
-import { NCLinkFullSync } from "./NCLinkFullSync.js";
-
 export function syncRoom(room) {
   return NCLinkFullSync({
     gate: room.gate,
     ist: room.ist,
     soll: room.soll,
-    route: "ODD-GATE-AXIS"
+    route: "GA-meKI-MIE-PULSE"
   });
 }
